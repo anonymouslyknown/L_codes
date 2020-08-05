f934c691f98b8b28750e62b7703e85959b03071b
Neetigya Soni <neetigya.soni02@gmail.com>
2020-08-06 00:21:27 +0530

    I made a dice.

---
+++ Dice.py
@@ -0,0 +1,28 @@
+import random
+import time
+ 
+min = 1
+max = 6
+
+def dice():
+    dice01 = 'y'
+    dice_number = input('How many dice you want?')
+    if dice_number=='1' :
+        while dice01=='y' or dice01 == "Y" or dice01 == "yes" :
+            print('Rolling dice...')
+            time.sleep(0.5)
+            print(random.randint(min,max))
+            dice01 = input('do you want to roll the  dice again?y/n')
+            if dice01 == "N" or dice01 == "n":
+                print("ok then!")
+    elif dice_number=='2':
+         while dice01=='y' or dice01 == "Y" or dice01 == "yes" :
+            print('Rolling dice...')
+            time.sleep(0.52)
+            print(random.randint(min,max))
+            print(random.randint(min,max))
+            dice01 = input('do you want to roll the dice again?y/n')
+            if dice01 == "N" or dice01 == "n":
+                print("ok then!")
+
+dice()
