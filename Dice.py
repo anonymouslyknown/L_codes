0d8fa56880d1c150ace83f0eddd696118a7cda8a
Neetigya Soni <neetigya.soni02@gmail.com>
2020-08-09 15:16:03 +0530

    changes in dice.

--- Dice.py
+++ Dice.py
@@ -6,13 +6,14 @@ max = 6
 
 def dice():
     dice01 = 'y'
-    dice_number = input('How many dice you want?')
+    dice_number = input('How many dice you want 1 or 2 ?')
     if dice_number=='1' :
-        while dice01=='y' or dice01 == "Y" or dice01 == "yes" :
+        while dice01=='y' or  dice01 == "yes" :
             print('Rolling dice...')
             time.sleep(0.5)
             print(random.randint(min,max))
             dice01 = input('do you want to roll the  dice again?y/n')
+            dice01 = dice01.lower()
             if dice01 == "N" or dice01 == "n":
                 print("ok then!")
     elif dice_number=='2':
@@ -22,7 +23,8 @@ def dice():
             print(random.randint(min,max))
             print(random.randint(min,max))
             dice01 = input('do you want to roll the dice again2?y/n')
-            if dice01 == "N" or dice01 == "n":
+            dice01 = dice01.lower()
+            if dice01 == "no" or dice01 == "n":
                 print("ok then!")
 
 dice()
